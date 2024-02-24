17878b61f8caef148cd75f60a3583e595a8e8a6c
alexmnsr <alexmnsr@vk.com>
2024-02-25 01:31:46 +0300

    update cmds, structurization code

--- commands/get_info_moderate.py
+++ commands/get_info_moderate.py
@@ -1,6 +1,7 @@
 from disnake.ext import commands
 from disnake import Embed, Color
 from database import execute_operation
+from commands.accesses import access_commands as is_access_command
 
 
 class GetInfoCog(commands.Cog):
@@ -9,7 +10,7 @@ class GetInfoCog(commands.Cog):
 
     @commands.command(name='moderators')
     async def get_moderators(self, ctx):
-        if self.is_access_command(ctx, 'moderators'):
+        if is_access_command(ctx, 'moderators'):
             try:
                 moderators_info = execute_operation('discord-esbot', 'select', 'moderator_servers',
                                                     columns='id_user, nickname_user, role_user',
@@ -29,13 +30,14 @@ class GetInfoCog(commands.Cog):
     @commands.command(name='exceptions_voice')
     async def exceptions_voice(self, ctx):
         try:
-            if self.is_access_command(ctx, 'exceptions_voice'):
+            if is_access_command(ctx, 'exceptions_voice'):
                 embed_content = ''
                 i_len = 0
                 exceptions = execute_operation('discord-esbot', 'select', 'servers_exceptions',
                                                columns='id, name_channel',
                                                where=f'`id_server`={ctx.guild.id}')
-                embed = Embed(title=f'Список исключений на {ctx.guild.name} (ID: {ctx.guild.id}):\n', color=Color.green())
+                embed = Embed(title=f'Список исключений на {ctx.guild.name} (ID: {ctx.guild.id}):\n',
+                              color=Color.green())
                 for exceptions_id in exceptions:
                     embed_content += f'Канал {exceptions_id["name_channel"]} (ID: {exceptions_id["id"]})\n'
                     i_len += 1
@@ -48,24 +50,8 @@ class GetInfoCog(commands.Cog):
         else:
             await ctx.send('Вы не имеете доступа к данной команде.')
 
-    def is_access_command(self, ctx, cmd):
-        role_access = execute_operation('discord-esbot', 'select', 'access_roles', columns='role',
-                                        where=f'`server_id`={ctx.guild.id}')
-        personal_access = execute_operation('discord-esbot', 'select', 'personal_access_cmd', columns='*',
-                                            where=f'`cmd`="{cmd}" AND `id_user`={ctx.author.id}')
-        moderators_info = execute_operation('discord-esbot', 'select', 'moderator_servers',
-                                            columns='id_user, role_user',
-                                            where=f'`id_server`={ctx.guild.id}')
-        for sys_admin in moderators_info:
-            if sys_admin == ctx.author.id:
-                return True
-        if personal_access and personal_access[0]['id_user'] == ctx.author.id:
-            return True
-        elif role_access and isinstance(role_access, list):
-            if personal_access and personal_access[0]['id_user'] == ctx.author.id:
-                return True
-            user_roles = set(role.name for role in ctx.author.roles)
-            db_roles = set(entry.get('role') for entry in role_access)
-            return bool(user_roles.intersection(db_roles))
-        else:
-            return False
+
+async def get_user_info(ctx, user_id):
+    """id: member.id, nick_user: member.name, ник на сервере: member.display_name"""
+    member = await ctx.guild.fetch_member(user_id)
+    return member
--- commands/online_users.py
+++ commands/online_users.py
@@ -2,6 +2,8 @@ from disnake.ext import commands
 from disnake import Embed, Color
 from database import execute_operation
 from datetime import datetime as dt
+from commands.accesses import access_commands as is_access_command
+from commands.get_info_moderate import get_user_info
 
 
 class OnlineCog(commands.Cog):
@@ -10,15 +12,18 @@ class OnlineCog(commands.Cog):
 
     @commands.command(name='access')
     async def access(self, ctx, cmd, user_id, sys=None):
-        if self.is_access_command(ctx, cmd):
-            user = await self.bot.fetch_user(user_id)
+        if is_access_command(ctx, cmd):
+            user = await get_user_info(ctx, user_id)
+            if not user:
+                await ctx.send(f'Пользователь с ID {user_id} не найден на сервере.')
+                return
             if sys == '-d':
                 execute_operation('discord-esbot', 'delete', table_name='personal_access_cmd',
                                   where=f'`cmd` = "{cmd}" AND `id_user` = {user_id} AND `server_id` = {ctx.guild.id}',
                                   commit=True)
                 embed = Embed(
                     title=f'Снятие личного доступа',
-                    description=f'Команда: /{cmd}\n↳ Пользователь: {user.name} (ID: {user_id})',
+                    description=f'Команда: /{cmd}\n↳ Пользователь: {user.display_name} (ID: {user_id})',
                     color=Color.red())
                 await ctx.send(embed=embed)
             else:
@@ -37,139 +42,211 @@ class OnlineCog(commands.Cog):
         else:
             await ctx.send('Вы не имеете доступа к данной команде.')
 
-    def is_access_command(self, ctx, cmd):
-        role_access = execute_operation('discord-esbot', 'select', 'access_roles', columns='role',
-                                        where=f'`server_id`={ctx.guild.id}')
-        personal_access = execute_operation('discord-esbot', 'select', 'personal_access_cmd', columns='*',
-                                            where=f'`cmd`="{cmd}" AND `id_user`={ctx.author.id}')
-        moderators_info = execute_operation('discord-esbot', 'select', 'moderator_servers',
-                                            columns='id_user, role_user',
-                                            where=f'`id_server`={ctx.guild.id}')
-        for sys_admin in moderators_info:
-            if sys_admin == ctx.author.id:
-                return True
-        if personal_access and personal_access[0]['id_user'] == ctx.author.id:
-            return True
-        elif role_access and isinstance(role_access, list):
-            if personal_access and personal_access[0]['id_user'] == ctx.author.id:
-                return True
-            user_roles = set(role.name for role in ctx.author.roles)
-            db_roles = set(entry.get('role') for entry in role_access)
-            return bool(user_roles.intersection(db_roles))
+    async def process_moderator_command(self, ctx, *args):
+        if len(args) < 3:
+            await ctx.send(
+                'Не указаны необходимые аргументы. Пример использования: /moderator user_id nick_name role (удалить: -d)')
+            return
+
+        sys_flag = args[-1] if args[-1] == "-d" else None
+        id_user = args[-3] if not sys_flag else args[-4]
+        nickname = args[-2] if not sys_flag else args[-3]
+        role = args[-1] if not sys_flag else args[-2]
+        if role == "SYS" and ctx.author.id != 479244541858152449:
+            await ctx.send(f'Данную роль может выдать только системный администратор (vk.com/alexmnsr)')
+            return
+        user_info = await get_user_info(ctx, id_user)
+        select_md = execute_operation('discord-esbot', 'select', 'moderator_servers',
+                                      columns='id_user, role_user',
+                                      where=f'`server_id`={ctx.guild.id}')
+        if sys_flag == '-d':
+            if not select_md:
+                await ctx.send(f'Пользователь с ID {id_user} не найден в списке модераторов.')
+                return
+            execute_operation('discord-esbot', 'delete', 'moderator_servers',
+                              where=f'`id_user`={id_user} AND `id_server`={ctx.guild.id}',
+                              commit=True)
+            embed = Embed(
+                title=f'Удаление модератора:',
+                description=f'↳ID: {id_user}\n↳Ник дискорда: {user_info.name}\n↳Ник на сервере: {user_info.display_name}',
+                color=Color.green())
+            await ctx.send(embed=embed)
         else:
-            return False
+            if select_md:
+                await ctx.send(f'Пользователь с ID {id_user} уже находится списке модераторов.')
+                return
+            values = {
+                'id_user': id_user,
+                'id_server': ctx.guild.id,
+                'nickname_user': nickname,
+                'role_user': role
+            }
+            execute_operation('discord-esbot', 'insert', 'moderator_servers', values=values,
+                              commit=True)
+        embed = Embed(
+            title=f'Добавление модератора:',
+            description=f'↳ID: {id_user}\n↳Ник дискорда: {user_info.name}\n↳Ник на сервере: {user_info.display_name}',
+            color=Color.green())
+        await ctx.send(embed=embed)
 
-    @commands.command(name='add_moderator')
-    async def add_moderator(self, ctx, id_user, nickname, role, sys=None):
-        if self.is_access_command(ctx, cmd='add_moderator'):
-            if sys == '-d':
-                execute_operation('discord-esbot', 'delete', 'moderator_servers',
-                                  where=f'`id_user`={id_user} AND `id_server`={ctx.guild.id}',
-                                  commit=True)
-                await ctx.send(f'Удалил модератора "{role}" (ID: {id_user})')
-            else:
-                values = {
-                    'id_user': id_user,
-                    'id_server': ctx.guild.id,
-                    'nickname_user': nickname,
-                    'role_user': role
-                }
-                execute_operation('discord-esbot', 'insert', 'moderator_servers', values=values,
-                                  commit=True)
-            await ctx.send(f'Добавил модератора "{role}" (ID: {id_user})')
-        else:
-            await ctx.send('Вы не имеете доступа к данной команде.')
 
-    @commands.command(name='add_exception')
-    async def add_exception(self, ctx, *id_channels, sys=None):
-        if self.is_access_command(ctx, cmd='add_exception'):
-            for id_channel in id_channels:
-                id_channel = int(id_channel)
-                name_channel = self.bot.get_channel(id_channel)
+@commands.command(name='moderator')
+async def moderator(self, ctx, *args):
+    if is_access_command(ctx, cmd='moderator'):
+        await self.process_moderator_command(ctx, *args)
+    else:
+        await ctx.send('Вы не имеете доступа к данной команде.')
 
-                if sys == '-d':
+
+async def process_add_exception_command(self, ctx, *args):
+    if len(args) < 1:
+        await ctx.send(
+            'Не указаны необходимые аргументы. Пример использования: /add_exception channel_id++ (удалить: -d)')
+        return
+    sys_flag = args[-1] if args[-1] == "-d" else None
+    channels = args[0:] if not sys_flag else args[:-1]
+
+    for channel_id in channels:
+        try:
+            id_channel = int(channel_id)
+            name_channel = self.bot.get_channel(id_channel)
+            select_except = execute_operation('discord-esbot', 'select', 'servers_exceptions',
+                                              columns='id',
+                                              where=f'`id`={id_channel} AND `id_server`={ctx.guild.id}')
+            if sys_flag and sys_flag == '-d':
+                if not select_except:
+                    await ctx.send(f'Такого канала не было найдено в исключениях. Список /exceptions')
+                    return
+                else:
                     execute_operation('discord-esbot', 'delete', 'servers_exceptions',
                                       where=f'`id`={id_channel} AND `id_server`={ctx.guild.id}',
                                       commit=True)
-                    await ctx.send(f'Удалил исключение каналу {name_channel.name} (ID: {id_channel})')
-                else:
-                    values = {
-                        'id': id_channel,
-                        'name_channel': name_channel.name,
-                        'id_server': ctx.guild.id
-                    }
-                    execute_operation('discord-esbot', 'insert', 'servers_exceptions', values=values,
-                                      commit=True)
-                    await ctx.send(f'Добавил исключение каналу {name_channel.name} (ID: {id_channel})')
-        else:
-            await ctx.send('Вы не имеете доступа к данной команде.')
-
-    @commands.command(name='access_roles')
-    async def access_role(self, ctx, cmd, *, role, sys=None):
-        if ctx.author.id == 479244541858152449:
-            if sys == '-d':
-                execute_operation('discord-esbot', 'delete', 'access_roles',
-                                  where=f'`role`="{role}" AND `server_id`={ctx.guild.id} AND `cmd`="{cmd}"',
-                                  commit=True)
-                embed = Embed(
-                    title=f'Снятие доступа по роли',
-                    description=f'Команда: /{cmd}\n↳ Роль: {role}',
-                    color=Color.red())
-                await ctx.send(embed=embed)
+                    embed = Embed(
+                        title=f'Удалил исключение:',
+                        description=f'↳ID канала: {id_channel}\n↳Название канала: {name_channel}',
+                        color=Color.green())
+                    await ctx.send(embed=embed)
             else:
+                if select_except:
+                    await ctx.send(f'Такой канал уже состоит в исключениях. Список /exceptions')
+                    return
                 values = {
-                    'role': role,
-                    'server_id': ctx.guild.id,
-                    'cmd': cmd
+                    'id': id_channel,
+                    'name_channel': name_channel.name,
+                    'id_server': ctx.guild.id
                 }
-                execute_operation('discord-esbot', 'insert', 'access_roles', values=values,
+                execute_operation('discord-esbot', 'insert', 'servers_exceptions', values=values,
                                   commit=True)
                 embed = Embed(
-                    title=f'Выдача доступа по роли',
-                    description=f'Команда: /{cmd}\n↳ Роль: {role}',
-                    color=Color.red())
+                    title=f'Добавил исключение:',
+                    description=f'↳ID канала: {id_channel}\n↳Название канала: {name_channel}',
+                    color=Color.green())
                 await ctx.send(embed=embed)
-        else:
-            await ctx.send('Вы не имеете доступа к данной команде.')
 
-    @commands.command(name='stats')
-    async def online_user(self, ctx, user_id=None, date=None):
-        if self.is_access_command(ctx, cmd='stats'):
-            if user_id is None:
-                user_id = ctx.author.id
-                date = dt.now().strftime("%d-%m-%Y")
-            query = execute_operation('discord-esbot', 'select', 'logs_users_time_on_voice',
-                                      columns='*',
-                                      where=f'`user_id`={user_id} AND `date` = \'{date}\' AND `id_server`={ctx.guild.id}')
+        except ValueError:
+            await ctx.send(f'Неверный формат ID канала: {channel_id}')
 
-            query_exception = execute_operation('discord-esbot', 'select', 'servers_exceptions', columns='id',
-                                                where=f'`id_server` = {ctx.guild.id}')
 
-            if not query:
-                await ctx.send(f'Нет статистики за {date} для пользователя (ID: {user_id})')
-                return
+@commands.command(name='exception')
+async def exception(self, ctx, *args):
+    if is_access_command(ctx, cmd='exception'):
+        await self.process_add_exception_command(ctx, *args)
+    else:
+        await ctx.send('Вы не имеете доступа к данной команде.')
 
-            channel_stats = {}
-            total_time = 0
 
-            for info_user in query:
-                if info_user['id_channel'] not in [exc['id'] for exc in query_exception]:
-                    real = info_user['time_leave_voice'] - info_user['time_start']
-                    total_time += real
+@commands.command(name='a_role')
+async def a_role(self, ctx, *args):
+    if is_access_command(ctx, cmd='a_role'):
+        await self.process_access_role(ctx, *args)
+    else:
+        await ctx.send('Вы не имеете доступа к данной команде.')
 
-                    channel_name = info_user["name_channel"]
-                    channel_stats[channel_name] = channel_stats.get(channel_name, 0) + real
 
-            embed = Embed(title=f'Статистика {query[0]["user_name"]} за {date}', color=Color.green())
+async def process_access_role(self, ctx, *args):
+    if len(args) < 2:
+        await ctx.send(
+            'Не указаны необходимые аргументы. Пример использования: /add_exception channel_id1 channel_id2 -d')
+        return
+    sys_flag = args[-1] if len(args) == 3 else None
+    role = args[-1] if not sys_flag else args[-2]
+    cmd = args[-2] if not sys_flag else args[-3]
+    select_role = execute_operation('discord-esbot', 'select', 'access_roles',
+                                    columns='role, cmd',
+                                    where=f'`server_id`={ctx.guild.id}')
+    if sys_flag and sys_flag == '-d':
+        if not select_role:
+            await ctx.send(f'Такая роль не имеет доступа к команде /{cmd}.')
+            return
+        execute_operation('discord-esbot', 'delete', 'access_roles',
+                          where=f'`role`="{role}" AND `server_id`={ctx.guild.id} AND `cmd`="{cmd}"',
+                          commit=True)
+        embed = Embed(
+            title=f'Снятие доступа по роли',
+            description=f'Команда: /{cmd}\n↳ Роль: {role}',
+            color=Color.red())
+        await ctx.send(embed=embed)
+    else:
+        if select_role:
+            await ctx.send(f'Такая роль уже имеет доступ к команде /{cmd}.')
+            return
+        values = {
+            'role': role,
+            'server_id': ctx.guild.id,
+            'cmd': cmd
+        }
+        execute_operation('discord-esbot', 'insert', 'access_roles', values=values,
+                          commit=True)
+        embed = Embed(
+            title=f'Выдача доступа по роли',
+            description=f'Команда: /{cmd}\n↳ Роль: {role}',
+            color=Color.red())
+        await ctx.send(embed=embed)
 
-            for channel, channel_time in channel_stats.items():
-                embed.add_field(name=f'В канале "{channel}":',
-                                value=f'{dt.utcfromtimestamp(channel_time).strftime("%H ч. %M м. %S с.")}',
-                                inline=False)
 
-            embed.add_field(name='Итоговое время в каналах:',
-                            value=f'{dt.utcfromtimestamp(total_time).strftime("%H ч. %M м. %S с.")}', inline=False)
+@commands.command(name='stats')
+async def online_user(self, ctx, user_id=None, date=None):
+    if is_access_command(ctx, cmd='stats'):
+        if user_id is None:
+            user_id = ctx.author.id
+            date = dt.now().strftime("%d-%m-%Y")
+        member = await get_user_info(ctx, user_id)
+        if not member:
+            await ctx.send(f'Пользователь с ID {user_id} не найден на сервере.')
+            return
+        query = execute_operation('discord-esbot', 'select', 'logs_users_time_on_voice',
+                                  columns='*',
+                                  where=f'`user_id`={user_id} AND `date` = \'{date}\' AND `id_server`={ctx.guild.id}')
 
-            await ctx.send(embed=embed)
-        else:
-            print("Не имеете доступа к данной команде")
+        query_exception = execute_operation('discord-esbot', 'select', 'servers_exceptions', columns='id',
+                                            where=f'`id_server` = {ctx.guild.id}')
+
+        if not query:
+            await ctx.send(
+                f'Нет статистики за {date} для пользователя  {member.name} "{member.display_name}" (ID: {user_id})')
+            return
+
+        channel_stats = {}
+        total_time = 0
+
+        for info_user in query:
+            if info_user['id_channel'] not in [exc['id'] for exc in query_exception]:
+                real = info_user['time_leave_voice'] - info_user['time_start']
+                total_time += real
+
+                channel_name = info_user["name_channel"]
+                channel_stats[channel_name] = channel_stats.get(channel_name, 0) + real
+
+        embed = Embed(title=f'Статистика {query[0]["user_name"]} за {date}', color=Color.green())
+
+        for channel, channel_time in channel_stats.items():
+            embed.add_field(name=f'В канале "{channel}":',
+                            value=f'{dt.utcfromtimestamp(channel_time).strftime("%H ч. %M м. %S с.")}',
+                            inline=False)
+
+        embed.add_field(name='Итоговое время в каналах:',
+                        value=f'{dt.utcfromtimestamp(total_time).strftime("%H ч. %M м. %S с.")}', inline=False)
+
+        await ctx.send(embed=embed)
+    else:
+        print("Не имеете доступа к данной команде")
