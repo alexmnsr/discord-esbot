95a6547eb888e5efa4a7129a46f987f3559faa6b
alexmnsr <alexmnsr@vk.com>
2024-03-02 20:32:33 +0300

    check_online

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 
 import config
 from utils.classes.bot import EsBot
+from dotenv import load_dotenv
+
+load_dotenv()
 
 bot = EsBot()
 
@@ -9,4 +12,6 @@ for filename in os.listdir('cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
 
-bot.run(config.discord)
+start = os.getenv('LOCAL_TOKEN') if os.getenv('DEBUG') == 'True' else os.getenv('TOKEN_BOT')
+
+bot.run(start)
