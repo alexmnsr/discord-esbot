a24a1e037f715eb42f31419a731665fb4531358d
alexmnsr <alexmnsr@vk.com>
2024-05-19 17:34:56 +0300

    change import mongodb

--- database.py
+++ database.py
@@ -1,15 +1,20 @@
+import os
+
 from motor.motor_asyncio import AsyncIOMotorClient
 
-import config
 from utils.classes.actions import Actions
 from utils.online.online import OnlineHandler
 from utils.punishments.punishments import PunishmentsHandler
 from utils.roles.roles import RolesHandler
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 class Database:
     def __init__(self, bot):
-        self.client = AsyncIOMotorClient(config.mongo)
+        self.client = AsyncIOMotorClient(os.getenv('MONGO_DB'))
         self.actions = Actions(self.client['Actions'])
         self.online_handler = OnlineHandler(self.client['Online'])
         self.punishments_handler = PunishmentsHandler(bot, self, self.client['Punishments'])
