d89252fbb5a4578c747147b4ef35ebb9dd314ff2
alexmnsr <118854735+alexmnsr@users.noreply.github.com>
2024-07-20 17:44:45 +0300

    fix online

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Устанавливаем временную зону
+ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+echo "Europe/Moscow" > /etc/timezone
+
 # Переходим в рабочую директорию вашего проекта внутри контейнера
 cd /bots/discord-esbot
 
