7f499787f6e15f56f6cc9d46e487b33d19372ae4
alexmnsr <118854735+alexmnsr@users.noreply.github.com>
2024-07-20 18:47:18 +0300

    fix online

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 RUN apt-get update && apt-get install -y tzdata
 
 ENV TZ=Europe/Moscow
-RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone # test
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
 # Переходим в рабочую директорию вашего проекта внутри контейнера
 cd /bots/discord-esbot
