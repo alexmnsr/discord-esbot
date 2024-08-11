b8cd8c1316ea76265579f580260f683da7c78cf5
alexmnsr <118854735+alexmnsr@users.noreply.github.com>
2024-08-11 21:57:43 +0300

    add cmd `delbuttons`, logging online

--- utils/online/online.py
+++ utils/online/online.py
@@ -42,7 +42,7 @@ class OnlineHandler:
     async def join(self, member: nextcord.Member,
                    channel, transition=False) -> None:
         if not transition:
-            log_channel, embed = self.send_embed_online(member=member, channel=channel, leave=True)
+            log_channel, embed = self.send_embed_online(member=member, channel=channel, join=True)
             await log_channel.send(embed=embed)
         await self.database.add_join_info(member, channel, is_counting(channel))
 
